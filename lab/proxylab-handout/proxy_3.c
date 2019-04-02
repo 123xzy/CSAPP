ae7193918b4a8caf51c6c2fe72146a7130e0c2b1
123xzy <1037884309@qq.com>
2019-04-02 05:26:55 -0700

    proxy lab:part three

---
+++ lab/proxylab-handout/proxy_3.c
@@ -0,0 +1,364 @@
+/*
+ * proxy_3.c
+ *
+ * author:xzy
+ * data:2019.4.2
+ */
+
+#include <stdio.h>
+#include "csapp.h"
+#include "sbuf.h"
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+#define MAX_BLOCK_SIZE 10
+
+/* max thread sizes */
+#define NTHREADS 4
+
+/* max buffer size */
+#define SBUFSIZE 16
+
+sbuf_t sbuf;/* shared buffer of connected descriptors */
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *connection = "Connection:close\r\nProxy-Connection:close\r\n";
+
+int conn_server(char *hostname,int *port,char *query_path);
+int parse_url(char *url, char *hostname,char *query_path,int *port);
+void *thread(void *vargp);
+
+typedef struct cache_block{
+	char cache_obj[MAX_OBJECT_SIZE];
+	char url[MAXLINE];
+	
+	int reader_cnt;	/* cnt of readers */
+	sem_t r_cnt_mutex;	/* mutex of reader_cnt */
+
+	int writer_cnt; /* cnt of writers */
+	sem_t w_cnt_mutex;	/* mutex of writer_cnt */
+	
+	struct cache_block *prev;
+	struct cache_block *next;
+
+}Cache_block;
+
+typedef struct cache{
+
+	Cache_block *head;
+	Cache_block *tail;
+
+	unsigned long len;
+
+}Cache;
+
+/* cache function */
+Cache *cache_create(void);
+Cache_block *cache_read(Cache *cache,char *url);
+void cache_LRU(Cache *cache,Cache_block *block);
+void cache_write(Cache *cache,char *url,char *buf);
+
+Cache *cache;
+
+int main(int argc, char **argv) 
+{
+    int i;
+    int listenfd, connfd;
+    char hostname[MAXLINE],port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+
+    /* Check command line args */
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    sbuf_init(&sbuf,SBUFSIZE);
+
+    cache = cache_create();
+
+    for(i = 0;i < NTHREADS;i++)
+   	Pthread_create(&tid,NULL,thread,NULL);
+ 	
+    while (1) {
+	clientlen = sizeof(clientaddr);
+
+	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept        
+    
+    	sbuf_insert(&sbuf,connfd);
+    }
+}
+
+
+/*
+ * handle with HTTP header from client request
+ * 1.Get the hostname,port of server to connect
+ * 2.Edit the HTTP header and send info to real server
+ */
+void *thread(void *vargp)
+{
+	Pthread_detach(pthread_self());
+	
+	char buf[MAXLINE];
+
+	// store the info of request
+	char method[MAXLINE],url[MAXLINE],version[MAXLINE];
+
+	char cache_buf[MAX_OBJECT_SIZE];
+
+	// parse the url and store info here
+	char hostname[MAXLINE],path[MAXLINE];
+	int port;
+
+	// rebuilt the http header 
+	char realserver_http_header[MAXLINE];
+	int real_serverfd;
+	rio_t server_rio;
+
+	rio_t proxy_rio;
+	
+	while(1){
+		int connfd = sbuf_remove(&sbuf);
+
+		Rio_readinitb(&proxy_rio,connfd);
+
+		// read the request info from client(Browser)
+		Rio_readlineb(&proxy_rio,buf,MAXLINE);
+		sscanf(buf,"%s%s%s",method,url,version);
+
+		// for test
+		printf("%s %s %s\n",method,url,version);
+
+		// proxy only support GET
+		if(strcasecmp(method,"GET")){
+			printf("Proxy only support 'GET'\n");
+			return;
+		}
+
+		Cache_block *block;
+
+		if((block = cache_read(cache,url)) != NULL){
+			strcmp(buf,block->cache_obj);
+			printf("info from cache\n");
+			Rio_writen(connfd,buf,strlen(buf));
+		}else{
+
+			parse_url(url,hostname,path,&port);
+	
+			// rebulit the header which will send to the real server
+			//built_http_header(realserver_http_header,hostname,path,port,&proxy_rio);
+
+			// connect to the end server
+			real_serverfd = conn_server(hostname,&port,path);
+			if(real_serverfd < 0){
+				printf("connected to real server failed\n");
+				return;
+			}	
+			Rio_readinitb(&server_rio,real_serverfd);
+			// write the http header to real server
+			Rio_writen(real_serverfd,realserver_http_header,strlen(realserver_http_header));
+
+			// receive message from real server and send to the client 
+			size_t n;
+			while((n = Rio_readlineb(&server_rio,buf,MAXLINE)) != 0){
+				printf("proxy received %d bytes\n",(int)n);
+				Rio_writen(connfd,buf,n);
+				strcat(cache_buf,buf);		
+			}
+
+			/* store it */
+			if(strlen(cache_buf) < MAX_OBJECT_SIZE){
+				cache_write(cache,url,cache_buf);
+			}else{
+				Free(cache_buf);
+			}
+
+		}
+	
+		Close(real_serverfd);
+		Close(connfd);
+	}
+}
+
+
+/* 
+ * fill the info of hostname,query_path,post about the specified url
+ */
+int parse_url(char *url,char *hostname,char *query_path,int *port)
+{
+	char *ptr_1;
+
+	// default port
+	*port = 8080;
+
+	// skip "http://" & "https://"
+	ptr_1 = strstr(url,"//"); 	
+	ptr_1 += 2;
+
+	char *ptr_2 = strstr(ptr_1,":");
+
+	if(ptr_2 != NULL){ // such as "www.xzy.com:8080/index.html"
+		*ptr_2 = '\0';
+		sscanf(ptr_1,"%s",hostname);
+		sscanf(ptr_2 + 1,"%d %s",port,query_path);
+	}else{ // such as "www.xzy.com/index.html
+		ptr_2 = strstr(ptr_1,"/");
+		if(ptr_2 != NULL) 
+		{
+			ptr_2[0] = '\0';
+			sscanf(ptr_1,"%s",hostname);
+			*ptr_2 = '/';
+			sscanf(ptr_2,"%s",query_path);
+		}else
+			sscanf(ptr_1,"%s",hostname);
+	}
+
+	// for test
+	printf("%s %s %s %d\n",url,hostname,query_path,*port);
+	
+	return;	
+}
+
+
+/* 
+ * connect to server,if failed return nagative num
+ * or return socker fd
+ */
+int conn_server(char *hostname,int *port,char *query_path)
+{
+	int clientfd;
+	char buf[MAXLINE];
+	rio_t rio;
+	char port_str[MAXLINE];
+	sprintf(port_str,"%d",*port);
+
+	printf("connecting to %s:%d\n",hostname,*port);
+	clientfd = Open_clientfd(hostname,port_str);	//built the connection to real server
+	Rio_readinitb(&rio,clientfd);
+
+	/* connection failed */
+	if(clientfd < 0){
+		printf("connection failed\n");
+		return clientfd;
+	}else{
+		printf("connection succ\n");
+	}
+
+	/* write request to server */
+	sprintf(buf,"GET %s HTTP/1.0\r\n",query_path);
+	Rio_writen(clientfd,buf,strlen(buf));
+	sprintf(buf,"Host:%s\r\n",hostname);
+	Rio_writen(clientfd,buf,strlen(buf));
+	Rio_writen(clientfd,user_agent_hdr,strlen(user_agent_hdr));
+	Rio_writen(clientfd,connection,strlen(connection));
+	Rio_writen(clientfd,"\r\n",strlen("\r\n"));
+
+	return clientfd;
+}
+
+
+/************************
+ * Cache function 
+ * **********************/
+
+Cache *cache_create()
+{
+	Cache *cache;
+
+	if((cache = Malloc(sizeof(Cache))) == NULL)
+		return NULL;
+
+	cache->head = cache->tail = NULL;
+	cache->len = 0;
+
+	return cache;
+}
+
+/* 
+ * find url in cache
+ * if not,return null
+ */
+Cache_block *cache_read(Cache *cache,char *url)
+{
+	Cache_block *block;
+
+	block = cache->head;
+
+	while(block != NULL)
+	{
+		if(strcasecmp(block->url,url))
+		{
+			cache_LRU(cache,block);
+			return block;
+		}
+		else
+			block = block->next;
+	}
+
+	return NULL;
+}
+
+/*
+ * rebulit the list
+ */
+void cache_LRU(Cache *cache,Cache_block *block)
+{
+	/* link the block to the tail of list */
+	block->prev->next = block->next;
+	block->next->prev = block->prev;
+	block->prev = cache->tail;
+	cache->tail->next = block;
+	block->next = NULL;
+	cache->tail = block;
+}
+
+
+/* 
+ * write new url into cache 
+ */
+void cache_write(Cache *cache,char *url,char *buf)
+{
+	Cache_block *block;
+
+	/* no block in cache */
+	if(cache->head->next == NULL)
+	{
+		block = Malloc(sizeof(struct cache_block));
+		
+		block->prev = block->next = NULL;
+		cache->head = cache->tail = block;
+		cache->len++;
+
+		sscanf(url,"%s",block->url); /*	copy url to cache block */
+		sscanf(buf,"%s",block->cache_obj); /* copy main boby to cache block */
+	}	
+	else{
+		block = Malloc(sizeof(struct cache_block));
+		sscanf(url,"%s",block->url); /*	copy url to cache block */
+		sscanf(buf,"%s",block->cache_obj); /* copy main boby to cache block */
+		
+		if(cache->len == MAX_BLOCK_SIZE)	/* too much blocks */
+		{
+			cache->head = cache->head->next;	
+			Free(cache->head->prev);
+			cache->head->prev = NULL;
+
+			block->prev = cache->tail;
+			cache->tail->next = block;
+			block->next = NULL;
+			cache->tail = block;
+		}else{			
+			block->prev = cache->tail;
+			cache->tail->next = block;
+			block->next = NULL;
+			cache->tail = block;
+			cache->len++;
+		}		
+	}
+}
